7c0e7fed5b73f4e7aebc45299edbfd58834dcdab
Boc <radu.boc@haufe-lexware.com>
2017-10-01 13:49:16 +0300

    added login

--- server/index.js
+++ server/index.js
@@ -4,6 +4,14 @@ var expressWs = require('express-ws')(app);
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var router = express.Router();
+var admin = require("firebase-admin");
+var serviceAccount = require("./key.json");
+var loginRoutes = require('./login');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://coffeequeue-1bf68.firebaseio.com"
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -22,7 +30,7 @@ app.use(function (req, res, next) {
     console.log('Client IP:', ip);
     next();
 });
-app.use('/chedy', function(req,res) {
+app.use('/chedy', function (req, res) {
     res.send('ceva');
 });
 router.ws('/', function (ws, req, res) {
@@ -31,14 +39,15 @@ router.ws('/', function (ws, req, res) {
     ws.on('message', function (msg) {
         //var plantMap = JSON.parse(msg);
         var message_to_client = {
-            data:"Connection with the server established"
-          }
-        ws.send(JSON.stringify(message_to_client)); 
+            data: "Connection with the server established"
+        }
+        ws.send(JSON.stringify(message_to_client));
     });
 });
 app.use('/chedyws', router);
-app.set('port', 80);
+app.use('/login', loginRoutes);
+app.set('port', 3000);
 
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
 });
